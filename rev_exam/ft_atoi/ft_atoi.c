dacf678cdef9c2a054d2a4e54df726166a05d734
ecnaraga <garance.lambey@gmail.com>
2023-03-05 13:02:50 +0100

    push ft_atoi/ft_atoi.c

---
+++ rev_exam/ft_atoi/ft_atoi.c
@@ -0,0 +1,60 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	unsigned int	n;
+
+	n = nb;
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		n = -nb;
+	}
+	if (n <= 9)
+		ft_putchar(n + '0');
+	else
+	{
+		ft_putnbr(n / 10);
+		ft_putchar((n % 10) + '0');
+	}
+}
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	s;
+	int	result;
+
+	i = 0;
+	s = 1;
+	result = 0;
+	while (str[i] && (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\r')))
+		i++;
+	if (str[i] && (str[i] == '+' || str[i] == '-'))
+	{
+		if (str[i] == '-')
+			s = -s;
+		i++;
+	}
+	while (str[i] && (str[i] >= '0' && str[i] <= '9'))
+	{
+		result = result * 10 + str[i] - '0';
+		i++;
+	}
+	return (result * s);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		ft_putnbr(ft_atoi(av[1]));
+	}
+	ft_putchar('\n');
+	return (0);
+}
